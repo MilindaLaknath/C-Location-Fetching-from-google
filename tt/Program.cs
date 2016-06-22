2ac90c2e9fc351b39691b6d1f96e4f7ccf9cf9b5
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2016-06-22 10:37:22 +0530

    Remove unused code. Improvements

--- tt/Program.cs
+++ tt/Program.cs
@@ -19,8 +19,6 @@ namespace tt
         {
             String lat = "6.908321";
             String lang = "79.917448";
-            //String url = "http://maps.googleapis.com/maps/api/geocode/json";
-            String urlParams = "?latlang=" + lat + "," + lang;
 
             getLocationAddr(lat, lang);
             
@@ -32,10 +30,7 @@ namespace tt
             String urlParams = "/maps/api/geocode/json?latlng=" + lat + "," + lang;
 
             var clientRest = new RestClient(new Uri(url));
-           // clientRest.BaseUrl = new Uri(url);
-
             var clientRequst = new RestRequest(new Uri(url+urlParams));
-            //clientRequst.Resource = new Uri(urlParams);
             
             clientRequst.Method = Method.GET;
             clientRequst.RequestFormat = DataFormat.Json;
@@ -44,10 +39,7 @@ namespace tt
             RootObject x = (RootObject)response.Data;
             Console.WriteLine(x.results[0].formatted_address);
 
-           // IRestResponse<RootObject> clientResponse = clientRest.Execute<RootObject>(clientRequst);
-           // System.IO.File.WriteAllText(@"C:\Users\Lahiru\Desktop\WriteText.txt", clientResponse.ToString());
-            //Console.WriteLine(clientResponse.ToString());
-            return "";
+            return null;
         }
 
         
